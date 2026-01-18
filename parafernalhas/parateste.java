bf514cf56d559338c9b5ebf6a9cf1c581bc98fde
Eluriomilmar <Lucasrfranca@hotmail.com>
2026-01-18 16:03:21 -0300

    Java - Nelio Alves

---
+++ parafernalhas/parateste.java
@@ -0,0 +1,14 @@
+package parafernalhas;
+
+public class parateste {
+
+    public static final double PI = 3.141592653589793238;
+
+    public static double circumference(double radius) {
+        return 2.0 * PI * radius;
+    }
+
+    public static double volume(double radius) {
+        return 4.0 * PI * radius * radius * radius / 3.0;
+    }
+}
--- teste.java
+++ teste.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
-
+import parafernalhas.parateste;
 
 public class teste {
     public static void main(String[] args){
-        for(int i = 0; i < 3; i++){
-            System.out.println(i);
-            i = 0;
-        }
+        Scanner sc = new Scanner(System.in);
+        double raio = sc.nextDouble();
+
+        System.out.printf("Circunferência: %.2f%n" +
+                "Volume: %.2f%n" +
+                "Pi: %.2f", parateste.circumference(raio), parateste.volume(raio), parateste.PI);
+
+        sc.close();
     }
 }
