8cf6ead83cc96b94e78c4e1908d661766f72e426
Eluriomilmar <Lucasrfranca@hotmail.com>
2026-01-21 19:21:14 -0300

    Java - Nelio Alves

---
+++ Ex0404.java
@@ -0,0 +1,32 @@
+/*
+Em um banco, para cadastrar uma conta bancária, é necessário informar o número da conta, o nome do titular da conta,
+e o valor de depósito inicial que o titular depositou ao abrir a conta. Este valor de depósito inicial, entretanto,
+é opcional, ou seja: se o titular não tiver dinheiro a ddepositar no momento de abrir sua conta, o depósito inicial
+não será feito e o saldo inicial da conta será, naturalmente, zero.
+
+Importante: Uma vez que uma conta bancária foi aberta, o número da conta nunca poderá ser alterado. Já o nome do titular
+pode ser alterado(pois uma pessoa pode mudar de nome por ocasião de um casamento, por exemplo).
+
+Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um mecanismo para proteger isso. O saldo só
+aumenta por meio de depósitos, e só diminui por meio de saques. Para cada saque realizado, o banco cobra uma taxa de
+R$ 5,00. Nota: A conta pode ficar com sado negativo se o saldo não for suficiente para realizar o saque e/ou pagar a taxa.
+
+Você deve faer um programa que realize o cadastro de uma conta, dando opção para que seja ou não informado o valor de
+depósito inicial. Em seguida, realizar um depósto e depois um saque, sempre mostrando os dados da conta após cada operação.
+ */
+
+import java.util.Scanner;
+import parafernalhas.ContaBanco;
+
+public class Ex0404 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String titular = sc.nextLine();
+        int conta = sc.nextInt();
+        float saldo = sc.nextFloat();
+        ContaBanco cliente = new ContaBanco(conta, titular, saldo);
+        cliente.deposito(1234.56);
+        cliente.saque(1234.56);
+        System.out.println(cliente.toString());
+    }
+}
---
+++ parafernalhas/ContaBanco.java
@@ -0,0 +1,55 @@
+package parafernalhas;
+
+public class ContaBanco {
+    private int numConta;
+    private String titular;
+    private double saldo;
+
+    public ContaBanco(int numConta, String titular, double saldo){
+        this.numConta = numConta;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public ContaBanco(int numConta, String titular){
+        this.numConta = numConta;
+        this.titular = titular;
+        this.saldo = 0;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    private void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void deposito(double valor){
+        setSaldo(this.getSaldo() + valor);
+    }
+
+    public void saque(double valor){
+        setSaldo(this.getSaldo() - valor);
+    }
+
+    public String toString() {
+        return "ContaBanco{" +
+                "numConta=" + numConta +
+                ", titular='" + titular + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
