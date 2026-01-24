793519528569a5cf1ed741ba4b92fd698d9bbb1c
Eluriomilmar <Lucasrfranca@hotmail.com>
2026-01-23 21:38:25 -0300

    Java - Nelio Alves

--- teste.java
+++ teste.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
+import parafernalhas.Produtos;
 
 public class teste {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int qtd = sc.nextInt();
         float total = 0;
-        double[] vetor = new double[qtd];
-        for(int i = 0; i < qtd ; i++){
-            vetor[i] = sc.nextFloat();
-            total += vetor[i];
+        Produtos[] vetor = new Produtos[qtd];
+        for (int i = 0; i < vetor.length ; i++) {
+            sc.nextLine();
+            System.out.printf("Nome: ");
+            String nome = sc.nextLine();
+            System.out.printf("Valor: ");
+            double valor = sc.nextDouble();
+            vetor[i] = new Produtos(nome, valor);
+        }
+        for(int i = 0 ; i < vetor.length ; i++) {
+            System.out.printf("Produto: %s  Preço: %.2f%n", vetor[i].getNome(), vetor[i].getPreco());
+            total += vetor[i].getPreco();
         }
 
-        System.out.printf("A média é igual a: %.2f", total/qtd);
+        System.out.printf("Média dos preços: %.2f", total/qtd);
+
+
+
 
         sc.close();
     }
+
 }
