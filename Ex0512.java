cae64490af760af62c87ea18485ae0597b2c0962
Eluriomilmar <Lucasrfranca@hotmail.com>
2026-01-26 16:10:48 -0300

    Java - Nelio Alves

---
+++ Ex0512.java
@@ -0,0 +1,37 @@
+/*
+A dona de um pensioanto possui dez quartos para alugar para estudantes, sendo esses quartos identificados por números(0-9).
+
+Faça um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o número de
+estudantes que vão alugar quartos(N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada
+registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu(de 0 a 9). Suponha
+que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato,
+por ordem de quarto.
+ */
+
+import java.util.Scanner;
+import parafernalhas.E0512;
+
+public class Ex0512 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Insira quantidade de quartos que serão alugados: ");
+        int qtd = sc.nextInt();
+        E0512[] quartos = new E0512[10];
+        for (int i = 0 ; i < qtd ; i++ ) {
+            sc.nextLine();
+            System.out.printf("%dº aluguel:%nNome:", i + 1);
+            String nome = sc.nextLine();
+            System.out.printf("Email: ");
+            String email = sc.nextLine();
+            System.out.printf("Número do quarto: ");
+            int quarto = sc.nextInt();
+            quartos[quarto] = new E0512(nome, email);
+        }
+
+        for (int i = 0; i < quartos.length ; i++) {
+            if (quartos[i] != null) {
+                System.out.printf("Quarto %d: %s, %s%n", i, quartos[i].getNome(), quartos[i].getEmail());
+            }
+        }
+    }
+}
--- teste.java
+++ teste.java
@@ -3,29 +3,8 @@ import parafernalhas.Produtos;
 
 public class teste {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int qtd = sc.nextInt();
-        float total = 0;
-        Produtos[] vetor = new Produtos[qtd];
-        for (int i = 0; i < vetor.length ; i++) {
-            sc.nextLine();
-            System.out.printf("Nome: ");
-            String nome = sc.nextLine();
-            System.out.printf("Valor: ");
-            double valor = sc.nextDouble();
-            vetor[i] = new Produtos(nome, valor);
-        }
-        for(int i = 0 ; i < vetor.length ; i++) {
-            System.out.printf("Produto: %s  Preço: %.2f%n", vetor[i].getNome(), vetor[i].getPreco());
-            total += vetor[i].getPreco();
-        }
-
-        System.out.printf("Média dos preços: %.2f", total/qtd);
-
-
-
-
-        sc.close();
+        double a = 1.123;
+        System.out.println(a.getClass());
     }
 
 }
