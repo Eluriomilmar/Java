fd97c8183f6e51ef9d7ba4accb5d568009042b78
Eluriomilmar <Lucasrfranca@hotmail.com>
2026-01-26 13:51:38 -0300

    Java - Nelio Alves

--- Ex0508.java
+++ Ex0508.java
@@ -1,6 +1,40 @@
 /*
-"Média pares"
- */
+Leia um vetor de N números inteiros. Em seguida, mostre na tela a média aritmética somente dos números pares lidos,
+com uma casa decimal. Se nenhum número par for digitado, mostrar mensagem: "Nenhum número par inserido".
+*/
+
+import java.util.Scanner;
 
 public class Ex0508 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Insira quantidade de elementos do vetor: ");
+        int qtd = sc.nextInt();
+        double pares = 0;
+        double total = 0;
+
+        while (qtd < 0) {
+            System.out.println("Insira quantidade maior ou igual a 0: ");
+            qtd = sc.nextInt();
+        }
+
+        int[] vetor = new int[qtd];
+
+        for (int i = 0 ; i < vetor.length ; i++) {
+            System.out.printf("Insira o elemento da posição %d: ", i);
+            vetor[i] = sc.nextInt();
+            if (vetor[i] % 2 == 0) {
+                pares += vetor[i];
+                total += 1;
+            }
+        }
+
+        if (total > 0) {
+            System.out.printf("Média dos pares: %.2f", pares/total);
+        } else {
+            System.out.printf("Nenhum número par inserido");
+        }
+        sc.close();
+    }
 }
--- Ex0509.java
+++ Ex0509.java
@@ -1,6 +1,40 @@
 /*
-"Mais velho"
+Fazer um programa para ler um conjunto de nomes de pessoas e suas respectivas idades. Os nomes devem ser armazenados
+em um vetor e as idades em outro vetor. Depois, mostrar na tela o nome da pessoa mais velha.
  */
 
+import java.util.Scanner;
+
 public class Ex0509 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Insira quantidade de pessoas a serem registradas: ");
+        int qtd = sc.nextInt();
+        String[] nomes = new String[qtd];
+        int[] idades = new int[qtd];
+        int indice_mais_velha = 0;
+
+
+        for (int i = 0 ; i < qtd ; i++) {
+            System.out.printf("Insira os dados da %dª pessoa%n", i + 1);
+            System.out.printf("Nome: ");
+            nomes[i] = sc.next();
+            System.out.printf("Idade: ");
+            idades[i] = sc.nextInt();
+            if (i == 0) {
+                indice_mais_velha = 0;
+            } else {
+                if (idades[i] > idades[indice_mais_velha]) {
+                    indice_mais_velha = i;
+                }
+            }
+        }
+
+        System.out.printf("A pessoa mais velha é: %s, que possui %d anos.", nomes[indice_mais_velha], idades[indice_mais_velha]);
+
+
+
+        sc.close();
+    }
 }
