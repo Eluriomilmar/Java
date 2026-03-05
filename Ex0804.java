fea6bb78bade6788e85d63a7c713f65e2831707a
Eluriomilmar <Lucasrfranca@hotmail.com>
2026-03-04 23:14:55 -0300

    Java - Nelio Alves

--- Ex0804.java
+++ Ex0804.java
@@ -5,7 +5,6 @@ import parafernalhas.E0804.PessoaFisica;
 import parafernalhas.E0804.PessoaJuridica;
 import parafernalhas.E0804.Contribuinte;
 
-
 public class Ex0804 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,15 +13,15 @@ public class Ex0804 {
 
         List<Contribuinte> list = new ArrayList<>();
 
-        for (int i = 1 ; i <= n ; i++) {
-            System.out.println("Tax payer #" +i + " data:");
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Tax payer #" + i + " data:");
             System.out.printf("Individual or company (I/C): ");
             char ch = sc.next().toUpperCase().charAt(0);
             if (ch == 'I') {
                 sc.nextLine();
                 System.out.printf("Name: ");
                 String name = sc.nextLine();
-                System.out.printf("Anual income: ");
+                System.out.printf("Annual income: ");
                 double annualIncome = sc.nextDouble();
                 System.out.printf("Health expenditures: ");
                 double healthExpenditures = sc.nextDouble();
@@ -31,7 +30,7 @@ public class Ex0804 {
                 System.out.printf("Name: ");
                 sc.nextLine();
                 String name = sc.nextLine();
-                System.out.printf("Anual income: ");
+                System.out.printf("Annual income: ");
                 double annualIncome = sc.nextDouble();
                 System.out.printf("Number of employees: ");
                 int numberOfEmployees = sc.nextInt();
