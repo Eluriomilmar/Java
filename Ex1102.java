cef072fba9b1d8e397647aaccb7038a51b730cd7
Eluriomilmar <Lucasrfranca@hotmail.com>
2026-03-15 11:30:12 -0300

    Java - Nelio Alves

--- Ex1102.java
+++ Ex1102.java
@@ -6,12 +6,8 @@ public class Ex1102 {
     public static void main(String[] args) {
 
         String path = "C:\\Users\\Lucas\\Desktop\\teste.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
 
             String line = br.readLine();
 
@@ -22,19 +18,6 @@ public class Ex1102 {
         }
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
